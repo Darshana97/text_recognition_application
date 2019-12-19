e64388542919e6ec64388f082cae14b7eb01147a
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2019-12-19 09:25:41 +0530

    text recognition application

---
+++ js practiceeee/server.js
@@ -0,0 +1,54 @@
+// const server = require('http');
+
+// server.createServer((req,res)=>{
+//     res.end('server is running');
+// }).listen(4000);
+
+
+
+// const server = require('http');
+// const express = require('express');
+
+// const app = express();
+
+// app.use((req,res,next)=>{
+//     res.send('this server is running');
+// });
+
+// server.createServer(app).listen(4000);
+
+
+const server = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+})
+
+app.get("/api/user", (req, res, next) => {
+
+    const details = [
+        {
+            id: 1,
+            name: '0ne',
+            password: 'abc'
+        },
+        {
+            id: 2,
+            name: 'two',
+            password: '123'
+        }
+
+    ];
+
+    res.json(details);
+
+});
+
+server.createServer(app).listen(4000);
